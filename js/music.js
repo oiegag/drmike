19f8b1faf172a83de2d49fb759f0b069476ba1e7
Mike McFadden <compositiongamesdev@gmail.com>
2013-01-11 22:35:44 -0500

    woops.. committed without music.js saved.

--- js/music.js
+++ js/music.js
@@ -3,7 +3,7 @@ var music = function () {
 	if (game.music_choice == 0) {
 	    return;
 	}
-	if (music.first_time) {
+	if (music.play_num == undefined) {
 	    music.first_time = false;
 	    music.play_num = game.music_choice;
 	    music.loaded[music.play_num] = new Audio(music.playable[music.play_num]);
@@ -18,7 +18,6 @@ var music = function () {
     }
 }
 
-music.first_time = true;
 music.play_num = undefined;
 music.loaded = [undefined,undefined,undefined,undefined];
 music.playable = ["","snd/song1.ogg","snd/song2.ogg","snd/song3.ogg"];
